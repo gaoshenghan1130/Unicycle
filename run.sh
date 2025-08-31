ce66d11565d25bdadd7e949fe5d35eac0583f2cc
gsh <gsh1130@sjtu.edu.cn>
2025-08-31 13:32:42 -0400

    import command update for Debug config

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-cmake -S . -B Debug -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=cubeide-gcc.cmake
+# cmake -S . -B Debug -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=cubeide-gcc.cmake
 
-cd Debug
-make
+# cd Debug
+# make
 
-cd ..
+# cd ..
 
 /opt/ST/STM32CubeCLT_1.19.0/STM32CubeProgrammer/bin/STM32_Programmer_CLI \
--c port=SWD -d Unicycle.elf
+-c port=SWD -d ./Debug/uc.elf
