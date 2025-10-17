da829fc0c300cb41e1670f165a501841253acb49
gsh <gsh1130@sjtu.edu.cn>
2025-10-16 21:43:44 -0400

    encoder for c

--- Core/Inc/BLE/ble.h
+++ Core/Inc/BLE/ble.h
@@ -3,6 +3,30 @@
 
 #include <stdint.h>
 
+
+#define DIR_POSITIVE 1
+#define DIR_NEGATIVE 0
+
+// encoding for direction
+#define DIR_X_CODE 0
+#define DIR_Y_CODE 1
+#define DIR_Z_CODE 2
+#define DIR_W_CODE 3
+
+#define DIR_CODE_LENGTH 3 // in bits for direction encoding, leave 1 bit for sign
+
+#define TORQUE_CODE_LENGTH 13 // in bits for torque encoding, unit A
+
+// first 3 digit of torque is exponent (2), next 10 digit is mantissa (10)
+#define TORQUE_EXP_LENGTH 3
+#define TORQUE_MANT_LENGTH 10
+
+#define TORQUE_FACTOR 0.1f // torque unit is 0.1A
+
+// in total 8 = 2 * 4 bytes for each direction
+
+// only using first 2 bytes for torque encoding, the rest is reserved, total 128 bits
+
 typedef struct
 {
     uint8_t pos;
@@ -24,16 +48,37 @@ typedef struct
     uint8_t size;      // actual data size
 } UCommandData_t;
 
+typedef struct 
+{
+    int8_t dirX;   // -100 ~ 100
+    int8_t dirY;   // -100 ~ 100
+    int8_t dirZ;   // -100 ~ 100
+    int8_t dirW;   // -100 ~ 100
+} Command_t;
+
 typedef struct BLEData
 {
     MainMotorData_t mainMotor;
     BalancerMotorData_t balancerMotor;
     UCommandData_t uCommand;
-} BLEData;
+} BLEData; // all data received from BLE
 
+
+
+
+// public functions
+Command_t getDirectionCommand();
+
+
+
+
+
+uint16_t numEncode(int torque, double factor); // use this to encode numbers to 2 bytes for transmission, for no 
+int numDecode(int torque, double factor); // use this to decode 2 bytes back to numbers
 void updateMotorData(char *rawData, int length);
 void updateBalancerData(char *rawData, int length);
 void loadCommand(char *data, int length);
 volatile UCommandData_t *getCommand();
+
 
 #endif /* BLE_H */
--- Core/Src/BLE/ble.c
+++ Core/Src/BLE/ble.c
@@ -1,5 +1,49 @@
 #include "../../Inc/BLE/ble.h"
 #include "../../../STM32_WPAN/App/custom_stm.h"
+#include <stdint.h>
+
+
+uint16_t numEncode(int torque, double factor)
+{
+    torque = (int)(torque / factor); // change to unit 0.1A
+    if (torque > 4095 || torque < -4095)
+        return 0xFFFF; // out of range
+
+    uint16_t absTorque = (torque < 0) ? -torque : torque;
+    uint8_t exponent = 0;
+
+    // find exponent
+    while (absTorque > 1023) // 10-bit mantissa
+    {
+        absTorque >>= 1;
+        exponent++;
+    }
+
+    // encode to 16-bit
+    uint16_t encoded = 0;
+    if (torque < 0)
+        encoded |= (1 << 13);      // sign bit: 13 th
+    encoded |= ((exponent & 0x07) << TORQUE_MANT_LENGTH); // exp: bit12~bit10
+    encoded |= (absTorque & 0x3FF);                       // mantissa: bit9~bit0
+
+    return encoded;
+}
+
+int numDecode(int torque, double factor)
+{
+    if (torque == 0xFFFF)
+        return 0; // error code
+
+    int sign = (torque & (1 << 13)) ? -1 : 1; // sign bit: 13 th
+    int exponent = (torque >> TORQUE_MANT_LENGTH) & 0x07; // exp: bit12~bit10
+    int mantissa = torque & 0x3FF;                        // mantissa: bit9~bit0
+
+    int decoded = mantissa << exponent; // reconstruct the number
+    decoded *= sign;
+    decoded = (int)(decoded * factor); // change back to A
+
+    return decoded;
+}
 
 volatile BLEData bleData = {
     .mainMotor = {
@@ -42,4 +86,31 @@ void loadCommand(char *data, int length)
         bleData.uCommand.rawData[i] = data[i];
     }
     bleData.uCommand.size = length;
+}
+
+Command_t getDirectionCommand()
+{
+    Command_t cmd = {0, 0, 0, 0};
+    for (int i=0; i < 16 && i < bleData.uCommand.size; i+=2) {
+        uint16_t val = (uint8_t)bleData.uCommand.rawData[i] | ((uint8_t)bleData.uCommand.rawData[i+1] << 8);
+        int decoded = numDecode(val, 1.0); // decode with factor 1.0
+        int dir = val >> 13 & 0x03; // get direction
+        switch (dir) {
+            case 0:
+                cmd.dirX = (decoded < -100) ? -100 : (decoded > 100) ? 100 : decoded;
+                break;
+            case 1:
+                cmd.dirY = (decoded < -100) ? -100 : (decoded > 100) ? 100 : decoded;
+                break;
+            case 2:
+                cmd.dirZ = (decoded < -100) ? -100 : (decoded > 100) ? 100 : decoded;
+                break;
+            case 3:
+                cmd.dirW = (decoded < -100) ? -100 : (decoded > 100) ? 100 : decoded;
+                break;
+            default:
+                break;
+        }
+    }
+    return cmd;
 }
